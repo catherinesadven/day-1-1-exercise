4a0661fa27cc531c34138b57e41b309f7c8fdfd6
Catherine <83837204+catherinesadven@users.noreply.github.com>
2021-08-03 16:34:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#Write your code below this line 👇
+print ("Hello world!")
+print ("Day 1 - Python Print Function")
+print ("The function is declared like this:")
+print ("print('what to print')")
+
+
+
+
+
